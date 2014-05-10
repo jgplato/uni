355c8475b089b790a53f0df3777af2003c5e56aa
Markus Fenske <iblue@gmx.net>
2014-05-10 12:09:34 +0200

    nummdiff and data

--- gp1/LIN/numdiff.rb
+++ gp1/LIN/numdiff.rb
@@ -1,6 +1,3 @@
-require "rubygems"
-require "byebug"
-
 %w(messung7 messung8).each do |file|
   content = File.open("./data/#{file}.txt"){|fh| fh.read}
   data = content.split("\n").map do |line|
@@ -11,11 +8,10 @@ require "byebug"
     t, x, v = line.split(/ +/).map{|val| val.sub(",",".").to_f}
 
     [t, x, v]
-  end
+  end.compact
 
   # Numerical differentiation: Replace values of v with new values
-  data.each_with_index do |index, (t, x, v)|
-    byebug
+  data.each_with_index do |(t, x, v), index|
     if v
       delta_t = data[index][0] - data[index-1][0]
       delta_x = data[index][1] - data[index-1][1]
@@ -28,7 +24,7 @@ require "byebug"
   # Output t vs v data for gnuplot
   File.open("./data/#{file}.dat", "w") do |fh|
     data.each do |(t, x, v)|
-      fh.write("#{t}\t#{v}\n") if v
+      fh.write("#{"%1.2f" % t}\t#{"%1.3f" % v}\n") if v
     end
   end
 end
